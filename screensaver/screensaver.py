f0086c283cb409e46070185502d1025686b8b706
Paul Schaack <paul.schaack2@gmail.com>
2020-10-07 14:23:23 +0100

    path helper functions added and better path joining methods added

--- screensaver/screensaver.py
+++ screensaver/screensaver.py
@@ -17,11 +17,9 @@ RSYNC_PORT = os.getenv('SCREENSAVER_RSYNC_PORT')
 # Global variable path validation
 if INPUT_PATH[-1] != '/':
     INPUT_PATH += '/'
-if OUTPUT_PATH[-1] == '/':
-    OUTPUT_PATH = OUTPUT_PATH[:-1]
 
 
-def get_photo_dirs_list() -> list:
+def get_remote_dirs_list() -> list:
     """
     Get list of directories containing photos from a remote server, whilst filtering some stuff
     using rsync command
@@ -42,7 +40,7 @@ def get_photo_dirs_list() -> list:
                            # from:
                            INPUT_PATH,
                            # to:
-                           OUTPUT_PATH
+                           os.path.join(OUTPUT_PATH, 'photos')
                            ],
                           # universal_newlines=True,  # This throws utf-8 error for some dirs
                           stdout=subprocess.PIPE)
@@ -60,6 +58,16 @@ def get_photo_dirs_list() -> list:
         photo_dirs_list.append(line.rstrip())
 
     return get_last_level_directories(photo_dirs_list)
+
+
+def get_local_dirs_list(path) -> list:
+    """
+    Get local list of directories of given path
+    :return: list with directory names
+    """
+    subdirs = [x[0] for x in os.walk(path)]
+    subdirs = get_last_level_directories(subdirs)
+    return subdirs
 
 
 def get_last_level_directories(photo_dirs_list) -> list:
@@ -84,29 +92,41 @@ def delete_old_screensaver_photos() -> None:
     :return: None
     """
     # First, check it exists by checking the return code of below command
-    proc = subprocess.run(["test", "-e", OUTPUT_PATH],
+    proc = subprocess.run(["test", "-e", os.path.join(OUTPUT_PATH, 'photos')],
                           # stdout=subprocess.PIPE,
                           stderr=subprocess.STDOUT
                           )
     if proc.returncode == 0:
-        subprocess.run(["rm", "-rf", OUTPUT_PATH],
+        subprocess.run(["rm", "-rf", os.path.join(OUTPUT_PATH, 'photos')],
                        # stdout=subprocess.PIPE,
                        stderr=subprocess.STDOUT
                        )
     pass
 
 
+def strip_path(path) -> str:
+    return os.path.basename(os.path.normpath(path))
+
+
+def strip_path_list(dir_list) -> list:
+    return [strip_path(x) for x in dir_list]
+
+
+def get_random_entry(dirs_list) -> str:
+    return np.random.choice(dirs_list)
+
+
 def upload_new_screensaver_photos(photo_dirs_list: list) -> None:
     """
     Select a random directory from INPUT_PATH and copy this locally to be
     picked up by the screensaver program
-    :param photo_dirs_list: a list of directory names inside paulito's photo directory
-                            the output from running  get_photo_dirs_list
+    :param photo_dirs_list: a list of directory names inside remote servers's directory
+                            the output from running get_remote_dirs_list
     :return: None
     """
     app_logger.info(f'Selecting a random choice from directory list')
-    random_dir_choice = np.random.choice(photo_dirs_list)
-    app_logger.info(f'Copying {random_dir_choice} locally to {OUTPUT_PATH}')
+    random_dir_choice = get_random_entry(photo_dirs_list)
+    app_logger.info(f'Copying {random_dir_choice} locally to ' + os.path.join(OUTPUT_PATH, 'photos'))
 
     delete_old_screensaver_photos()
 
@@ -128,16 +148,16 @@ def upload_new_screensaver_photos(photo_dirs_list: list) -> None:
                            # exclude files:
                            "--exclude", "*",
                            # from:
-                           f"{INPUT_PATH}{random_dir_choice}",
+                           os.path.join(INPUT_PATH, random_dir_choice),
                            # to:
-                           OUTPUT_PATH],
+                           os.path.join(OUTPUT_PATH, 'photos')],
                           stdout=subprocess.PIPE,  # required to return names of photos transferred
                           universal_newlines=True  # required to return string, not bytes-like obj
                           # stderr=subprocess.STDOUT
                           )
 
     # Change mode of newly created directory so photos can be deleted when job runs
-    subprocess.run(["chmod", "777", OUTPUT_PATH])
+    subprocess.run(["chmod", "777", os.path.join(OUTPUT_PATH, 'photos')])
 
     # Lower case output to handle cases when file extension is in caps
     proc_stdout_lower_case = proc.stdout.lower()
@@ -150,10 +170,32 @@ def upload_new_screensaver_photos(photo_dirs_list: list) -> None:
 
 def main() -> None:
     app_logger.info('Getting photo directories list from paulito')
-    photo_dirs_list = get_photo_dirs_list()
+    remote_list = get_remote_dirs_list()
     app_logger.info('Starting upload_new_screensaver_photos')
-    upload_new_screensaver_photos(photo_dirs_list)
+    upload_new_screensaver_photos(remote_list)
     app_logger.info('Finished running screensaver.py!')
+
+    ##
+    #1 check if paulito can be reached. get dir_list. if all of dir_list in already_used, remove all from already_used list.
+    #2 choose random directory
+    #3 load already_used pickle, and check if it is in there. if yes, choose new one & repeat
+    #4 check if dir exists localy
+    #  if yes, copy locally from lib, then rsync server with lib after
+    #  if not, rysync to screensaver.
+    #        check size of lib. if above 15GBP, delete random directory and repeat until <15GB.
+    #        then copy to lib (make new dir)
+    #5 add dir to already used pickle and save
+    #1---> if paulito cannot be reached, choose random folder from list. if all of local in already_used, remove all from already_used list.
+    # check if folder in already_used_list. if yes, repeat, until not, then copy to screensaver
+
+    local_list = get_local_dirs_list(os.path.join(OUTPUT_PATH, 'library'))
+    a = strip_path_list(local_list)
+    b = strip_path_list(remote_list)
+    print(get_random_entry(local_list))
+    print(strip_path(get_random_entry(local_list)))
+    print(set(a).issubset(b))
+
+    ##
     pass
 
 
